391190b14c7d72663348c0ba5234eb6dc74e02f7
ZakariaICT <99720725+ZakariaICT@users.noreply.github.com>
2024-10-23 15:53:08 +0200

    added CommentService

--- BoardService/BoardService/Program.cs
+++ BoardService/BoardService/Program.cs
@@ -4,8 +4,19 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Voeg CORS-configuratie toe
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseInMemoryDatabase("TaskBoardDB"));  // Gebruik een echte SQL database als nodig.
@@ -25,8 +36,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Gebruik de CORS-policy
+app.UseCors("AllowAllOrigins");
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
