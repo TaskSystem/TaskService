e1ea8ac9022ee4898d1a9d4d2d4137c4c807f76b
ZakariaICT <99720725+ZakariaICT@users.noreply.github.com>
2024-10-24 23:41:06 +0200

    changes program and docker compose

--- TaskSystem/TaskService/Program.cs
+++ TaskSystem/TaskService/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddSingleton<ITaskRepository, TaskRepository>();
 builder.Services.AddControllers();
+builder.Services.AddSingleton<PublisherService>();
 
 // Swagger configuratie
 builder.Services.AddEndpointsApiExplorer();
